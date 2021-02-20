8427a2c59c449f4d3b7ed460477b2f106e4000cb
chris-hayashi <Chrishayashi@rocketmail.com>
2021-02-19 16:37:07 -0800

    Uploading Unit 2 assignment (Flix part 2)

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christopher Hayashi on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
